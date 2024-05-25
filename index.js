3faf48f0358fb1d6bc9fe5525f10466c2315f252
Roman-14 <sevenagondragon@gmail.com>
2024-05-25 16:54:26 +0100

    Bar experiment

--- index.js
+++ index.js
@@ -44,3 +44,12 @@ function logoTyping() {
 };
 
 logoTyping();
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+});
